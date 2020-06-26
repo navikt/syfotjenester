fb95fcf98a7d2d177347369b1d4552e2a37262a6
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-06-26 13:20:40 +0200

    Fix package name of LocalDateTimeXMlAdapter

--- aktorid-v2/src/main/wsdl/no/nav/tjeneste/virksomhet/aktoer/v2/adapter/LocalDateTimeXmlAdapter.java
+++ aktorid-v2/src/main/wsdl/no/nav/tjeneste/virksomhet/aktoer/v2/adapter/LocalDateTimeXmlAdapter.java
@@ -1,7 +1,6 @@
-package no.nav.tjeneste.adapter;
+package no.nav.tjeneste.virksomhet.aktoer.v2.adapter;
 
 import com.migesok.jaxb.adapter.javatime.TemporalAccessorXmlAdapter;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
